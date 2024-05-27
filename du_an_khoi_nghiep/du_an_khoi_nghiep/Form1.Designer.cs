cc61bac8e14839e55de37aabe3f38e47734cf3c0
nguyễn mạnh <manharia123@gmail.com>
2024-05-28 02:18:56 +0700

    mạnh làm form 1

--- du_an_khoi_nghiep/du_an_khoi_nghiep/Form1.Designer.cs
+++ du_an_khoi_nghiep/du_an_khoi_nghiep/Form1.Designer.cs
@@ -29,22 +29,13 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            pictureBox1 = new PictureBox();
             button1 = new Button();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button2 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
-            pictureBox1.Image = Properties.Resources.micro;
-            pictureBox1.InitialImage = null;
-            pictureBox1.Location = new Point(39, 12);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(320, 271);
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
             // 
             // button1
             // 
@@ -53,12 +44,54 @@
             button1.BackColor = Color.FromArgb(255, 255, 128);
             button1.BackgroundImageLayout = ImageLayout.Zoom;
             button1.ForeColor = Color.Black;
-            button1.Location = new Point(39, 289);
+            button1.Location = new Point(277, 274);
             button1.Name = "button1";
-            button1.Size = new Size(320, 43);
+            button1.Size = new Size(222, 43);
             button1.TabIndex = 1;
             button1.Text = "Kết nối Bluetooth";
             button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(320, 31);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(148, 174);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.Location = new Point(332, 226);
+            label1.Name = "label1";
+            label1.Size = new Size(120, 21);
+            label1.TabIndex = 3;
+            label1.Text = "micro đa năng";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(627, 12);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(146, 58);
+            textBox1.TabIndex = 4;
+            textBox1.Text = "1 sản phẩm độc quyền của công ty khởi nghiệp ABC";
+            // 
+            // button2
+            // 
+            button2.ForeColor = SystemColors.ActiveCaptionText;
+            button2.Location = new Point(651, 392);
+            button2.Name = "button2";
+            button2.Size = new Size(107, 46);
+            button2.TabIndex = 5;
+            button2.Text = "Thoát";
+            button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -67,9 +100,12 @@
             BackColor = SystemColors.ButtonHighlight;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(395, 420);
-            Controls.Add(button1);
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
             Controls.Add(pictureBox1);
+            Controls.Add(button1);
             ForeColor = SystemColors.Control;
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
@@ -77,11 +113,14 @@
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-
-        private PictureBox pictureBox1;
         private Button button1;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private TextBox textBox1;
+        private Button button2;
     }
 }
--- du_an_khoi_nghiep/du_an_khoi_nghiep/Form1.cs
+++ du_an_khoi_nghiep/du_an_khoi_nghiep/Form1.cs
@@ -1,4 +1,4 @@
-namespace du_an_khoi_nghiep
+﻿namespace du_an_khoi_nghiep
 {
     public partial class Form1 : Form
     {
@@ -8,6 +8,26 @@ namespace du_an_khoi_nghiep
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("kết nối với thiết bị thành công ,Bạn có muốn xem hướng dẫn sử dụng không?", caption: "kết nối thành công", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                huongdan formhd = new huongdan();
+                formhd.ShowDialog();
+            }
+            else
+            {
+                huongdan2cs form2 = new huongdan2cs();
+                form2.ShowDialog();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
