4c6868e84bf2e2e26940ec7d44d72a52a605d1e8
nguyễn mạnh <manharia123@gmail.com>
2024-05-28 02:20:25 +0700

    mạnh làm form hướng dẫn 2

---
+++ du_an_khoi_nghiep/du_an_khoi_nghiep/huongdan2cs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace du_an_khoi_nghiep
+{
+    public partial class huongdan2cs : Form
+    {
+        public huongdan2cs()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form4 form4 = new Form4();
+            form4.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form6 form6 = new Form6();
+            form6.ShowDialog();
+        }
+    }
+}
